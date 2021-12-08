00d3b07c6b73e791c532ec6600dc7998c1650a93
Steve McGhee <steve@mcgheemail.com>
2021-12-08 08:10:13 -0800

    skeleton creation

---
+++ topics-to-files.sh
@@ -0,0 +1,15 @@
+cat topics.txt | while read line;
+    do 
+        # echo "##$line##"  # debug
+        [[ ! "$line" ]] && continue;
+        line=$(echo $line | tr \/ _); # drop / marks because file system
+        line="${line/\&/_}"; # drop & marks because unix system
+        line="${line/\%/_}"; # drop % marks because just in case?
+        line="${line/\+/_}"; # drop + marks because URL?
+        line="${line/\"/_}"; # drop " marks because URLs
+        line="${line// /_}"; # drop spaces because URLs
+        line="${line//(/_}"; # drop parens because URLs
+        line="${line//)/_}"; # drop parens because URLs
+        line="${line//__/_}"; # dedupe underscores after all that
+        echo cp empty_topic.md docs/${line}.md
+    done
